99249b1d0a590288cbcbe3a63e3811f279e2f2b4
patorrad <paolo.a.torrado@gmail.com>
2020-01-07 08:34:51 -0800

    Completed JS

---
+++ app.js
@@ -0,0 +1,9 @@
+let Employee = require("./lib/Employee");
+let Engineer = require("./lib/Engineer");
+let Manager  = require("./lib/Manager");
+let Intern   = require("./lib/Inter");
+let fs = require("fs");
+
+const e = new Employee();
+
+console.log(e);
